be95617542636b44ae12476175f7f185ea1d67bc
amiratak88@gmail.com <amiratak88@gmail.com>
2018-06-28 20:49:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 function shout(str) {
-  return str.toUpperCase()
+  return str.toUpperCase();
 }
+
+function whisper(str) {
+  return str.toLowerCase();
+}
+
+function logShout(str) {
+  console.log(shout(str));
+}
+
+function logWhisper(str) {
+  console.log(whisper(str));
+}
+
+function sayHiToGrandma(str) {
+  if (str.toLowerCase() === str) {
+    return "I can't hear you!";
+  } else if (str.toUppercase() === str) {
+    return "YES INDEED!";
+  } else if (str == "")
+}
+
